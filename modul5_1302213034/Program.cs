4706ff4e006890cb088fec485ac15b2297fef1b6
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-03-24 09:10:00 +0700

    menambahkan class SimpleDataBase

--- modul5_1302213034/Program.cs
+++ modul5_1302213034/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using modul5_1302213034;
+
+SimpleDataBase<int> Data = new SimpleDataBase<int>();
+Data.AddNewData_1302213034(13);
+Data.AddNewData_1302213034(02);
+Data.AddNewData_1302213034(21);
+Data.printAllData();
---
+++ modul5_1302213034/SimpleDataBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213034
+{
+    internal class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData_1302213034(T dataBaru)
+        {
+            storedData.Add(dataBaru);
+            inputDates.Add(DateTime.Now);
+        }
+
+        public void printAllData()
+        {
+            for (int i = 0; i < storedData.Count; i++)
+            {
+                Console.WriteLine("Data " + (i + 1) + " berisi: " + storedData[i]
+                    + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+            }
+        }
+    }
+}
