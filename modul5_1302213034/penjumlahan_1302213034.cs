e93ce253a4e1a21d44d43a005b79dfef359850f7
herdiardn <121550473+herdiardn@users.noreply.github.com>
2023-03-24 08:32:44 +0700

    “menambahkan class Penjumlahan dengan method JumlahTigaAngka

---
+++ modul5_1302213034/penjumlahan_1302213034.cs
@@ -0,0 +1,20 @@
+﻿using modul5_1302213034;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_1302213034
+{
+    internal class penjumlahan_1302213034
+    {
+        public dynamic JumlahTigaAngka_1302213034<T> (T angka1, T angka2, T angka3)
+        { return ((dynamic)angka1 + (dynamic)angka2 + (dynamic)angka3);
+        //dynamic hasil= null;
+           // hasil =(dynamic)angka1 + (dynamic)angka2 + (dynamic)angka3;
+            //return hasil;
+        }
+    }
+}
+
